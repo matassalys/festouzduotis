0f97149dbb8c5373d42d0e057e9b23daf2854544
Matas Salys <matsal@ktu.lt>
2021-07-27 19:07:06 +0300

    initial commit
    initial commit for spring

---
+++ src/main/java/com/example/festoassignment/FestoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.festoassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestoAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestoAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/festoassignment/FestoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festoassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestoAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
